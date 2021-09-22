7a010b08053c9497d81d491e8eab0a7abb2bf3a3
johnnyloreano <laureano.pedrojoao@gmail.com>
2021-09-22 20:26:02 -0300

    SErver Up e com Swagger automatico

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express, { Application, Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+
+import Router from "./routes";
+
+const PORT = process.env.PORT || 8000;
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  })
+);
+
+app.use(Router);
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
